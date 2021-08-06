5b2aaaba219f40afd7c63e048162098e6063b027
Dmitry Vasilevsky <dmitry.vasilevsky@immelman.ru>
2021-08-07 00:12:34 +0300

    Добавлен pug

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,5 +68,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-property-in-object', { loose: true }]],
+    }
   }
 }
